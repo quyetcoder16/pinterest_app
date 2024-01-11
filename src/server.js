10b38f962fb33115e2cda7d7d44ab564393a69f2
dxquyet16 <quyetcoder16@gmail.com>
2024-01-11 09:21:47 +0700

    init project

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "PUT", "POST", "DELETE"]
+}));
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log("server is running on port : ", PORT);
+});
