e8c0e6cbcc8b0548a6890048ea0b2b1e7bd035ac
quyetcoder16 <129844186+quyetcoder16@users.noreply.github.com>
2024-01-15 07:36:34 +0700

    done api Comment (#4)
    * error commit
    
    * fix data success
    
    * done comment
    
    * fix conflict

--- src/routers/comment.router.js
+++ src/routers/comment.router.js
@@ -1,5 +1,7 @@
 import express from "express";
-import { getCommentByImageId, postRecordComment } from "../controllers/comment.controller.js";
+
+import { deleteComment, getCommentByImageId, postRecordComment } from "../controllers/comment.controller.js";
+
 import { authTokenMiddleware } from "../middlewares/auth.middleware.js";
 import { checkUserId } from "../middlewares/checkUserId.middleware.js";
 
@@ -7,5 +9,7 @@ const commentRouter = express.Router();
 
 commentRouter.get("/get-comment-by-imageId/:imageId", getCommentByImageId);
 commentRouter.post("/post-record-comment", authTokenMiddleware, checkUserId, postRecordComment);
+commentRouter.delete("/delete-comment", authTokenMiddleware, checkUserId, deleteComment);
+
 
 export default commentRouter;
